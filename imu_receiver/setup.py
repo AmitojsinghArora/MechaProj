f5e6b28194f83013de0420dcd7b76813519e38bb
Amitojsingh Arora <amitojsingh.arora@alumni.fh-aachen.de>
2025-02-11 14:29:26 +0100

    Refactor IMUReceiverNode to use JSON parsing for received data

---
+++ imu_receiver/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'imu_receiver'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='wilherm',
+    maintainer_email='wilherm@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'imu_udp_receiver = imu_receiver.imu_udp_receiver:main',
+        ],
+    },
+)
